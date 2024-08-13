ee46a5ebf75ecf51881fbd5b13aee82ecd925de8
Prince-Majestal <mathieuog@gmail.com>
2024-08-13 06:06:31 -0300

    Put the js and html files in their own directories and defined the default route to show the webpage

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,5 @@
 document.getElementById("searchForm").addEventListener('submit', function(event) {
     event.preventDefault();
-    console.log("Submit button pressed");
     const fields = document.querySelectorAll(".Query");
     let query = 'https://www.googleapis.com/books/v1/volumes?';
     let firstField = true;
